46b6fba4c425116c2053581bad04525f7bc8a4b8
patinas <andreas.patinas@gmail.com>
2022-10-18 10:38:51 +0200

    Last Sync: 2022-10-18 10:38:51

--- backup.sh
+++ backup.sh
@@ -18,7 +18,7 @@ CHANGES_EXIST="$(git status --porcelain | wc -l)"
 if [ "$CHANGES_EXIST" -eq 0 ]; then
 	exit 0
 fi
-git pull
+git pull origin main
 git add .
 git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S")"
 git push origin HEAD:main
